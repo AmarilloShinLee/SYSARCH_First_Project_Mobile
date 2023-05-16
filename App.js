7036fe5c1ca61e839882fb4ff18955ea6b7f9594
Gabriel Alpha Aguilar <gabrielalpha98@gmail.com>
2023-05-16 09:02:18 +0800

    First Commit for the First React Mobile App

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
+
+const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+        style={styles.image}
+        source="https://picsum.photos/seed/696/3000/2000"
+        //placeholder={blurhash}
+        contentFit="cover"
+        transition={1000}
+      />
+      <Text>Hello World! It's a ya boi! Amarillo Shin Lee</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +26,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#0553',
   },
 });
